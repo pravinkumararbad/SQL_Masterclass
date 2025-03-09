12d65fe02755b08e83e159b9e33c1850a729d732
Pravinkumar Arbad <pravinkumar.arbad@gmail.com>
2025-03-09 22:12:31 +0530

    This commit handles all about the AVG aggregate function and exercises on the section.

---
+++ 7. Aggregate Functions/6. Aggregate_Function_Exercises.sql
@@ -0,0 +1,28 @@
+-- ----------------------------------------------- AGGREGATE FUNCTIONS EXERCISES -------------------------------------------- --
+USE book_shop;
+SELECT database();
+
+-- Print the number of books in the database
+SELECT COUNT(*) FROM books;
+
+-- Print out how many books were released in each year
+SELECT released_year, COUNT( title ) AS books_released FROM books GROUP BY released_year ORDER BY released_year;
+
+-- Print out the total number of books in stock
+SELECT SUM( stock_quantity ) AS n_books FROM books;
+
+-- Find the average released_year for each author
+SELECT CONCAT (author_fname, ' ', author_lname) AS auth_fullname, AVG (released_year) AS avg_rel_yr_for_auth FROM books GROUP BY author_fname, author_lname;
+
+-- Find the full name of the author who wrote the longest book by title length and page count.
+SELECT CONCAT( author_fname, ' ', author_lname ) AS writer FROM books WHERE ( SELECT MAX( CHAR_LENGTH( title ) ) FROM books ) = CHAR_LENGTH( title );
+SELECT CONCAT( author_fname, ' ', author_lname ) AS writer FROM books WHERE pages = ( SELECT MAX( pages ) FROM books );
+
+SELECT CONCAT( author_fname, ' ', author_lname ) AS writer FROM books ORDER BY CHAR_LENGTH( title ) DESC LIMIT 1;
+SELECT CONCAT( author_fname, ' ', author_lname ) AS writer FROM books ORDER BY pages DESC LIMIT 1;
+SELECT * FROM books;
+
+-- Make this happen
+-- Year #Books avg_pages
+SELECT released_year AS Year, COUNT( title ) AS '#Books', AVG ( pages ) avg_books FROM books GROUP BY released_year ORDER BY released_year ASC;
+SELECT released_year, pages FROM books GROUP BY released_year ORDER BY released_year ASC;
